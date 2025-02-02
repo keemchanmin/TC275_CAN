07880744d5abdff44cbb37dfffb2b37e230def78
chanmin <1208cksals@naver.com>
2025-02-02 13:33:04 +0900

    TC275 CAN

---
+++ TC275_CAN/TriCore Debug (TASKING)/Libraries/Service/CpuGeneric/SysSe/Bsp/Assert.d
@@ -0,0 +1,20 @@
+Assert.o :	../Libraries/Service/CpuGeneric/SysSe/Bsp/Assert.c
+../Libraries/Service/CpuGeneric/SysSe/Bsp/Assert.c :
+Assert.o :	..\Libraries\Service\CpuGeneric\SysSe\Bsp\Assert.h
+..\Libraries\Service\CpuGeneric\SysSe\Bsp\Assert.h :
+Assert.o :	"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h"
+"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\Service\\CpuGeneric\_Utilities\Ifx_Assert.h" :
+Assert.o :	"C:\\Users\\A\\aurix_final\\TC275_CAN\\Configurations\Ifx_Cfg.h"
+"C:\\Users\\A\\aurix_final\\TC275_CAN\\Configurations\Ifx_Cfg.h" :
+Assert.o :	"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h"
+"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_Types.h" :
+Assert.o :	"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h"
+"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h" :
+Assert.o :	"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h"
+"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h" :
+Assert.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+Assert.o :	"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h"
+"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Platform_Types.h" :
+Assert.o :	"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h"
+"C:\\Users\\A\\aurix_final\\TC275_CAN\\Libraries\\iLLD\\TC27D\\Tricore\Cpu\Std\Ifx_TypesTasking.h" :
